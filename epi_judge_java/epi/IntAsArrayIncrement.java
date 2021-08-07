44cebbe631d549b21d6b11a5d4ebc82c73ca60fb
jiangting <jiang.ting@northeastern.edu>
2021-08-07 10:23:30 -0700

    "PlusOne"

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -7,7 +7,33 @@ public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
     // TODO - you fill in here.
-    return null;
+
+    int n = A.size()-1 ;
+    A.set(n, A.get(n) +1);
+
+    for (int i = n; i > 0 && A.get(i) == 10 ; i--) {
+      A.set(i, 0);
+      A.set(i-1, A.get(i-1) +1);
+    }
+
+    if (A.get(0) == 10) {
+      A.set(0,0);
+      A.add(0, 1);
+    }
+
+//    Time: O(n)  Space:O(1)
+
+
+
+
+
+
+
+
+
+
+
+    return A;
   }
 
   public static void main(String[] args) {
