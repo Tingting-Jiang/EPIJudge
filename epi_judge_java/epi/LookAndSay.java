92711575b13bc6d0fcbb6db09213e745faf6026a
jiangting <jiang.ting@northeastern.edu>
2021-08-12 10:18:00 -0700

    "lookAndSay"

--- epi_judge_java/epi/LookAndSay.java
+++ epi_judge_java/epi/LookAndSay.java
@@ -6,7 +6,26 @@ public class LookAndSay {
 
   public static String lookAndSay(int n) {
     // TODO - you fill in here.
-    return "";
+    String ans = "1";
+    for (int i = 1; i < n; i++) {
+      ans = getNext(ans);
+    }
+    return ans;
+
+  }
+
+  private static String getNext(String ans) {
+    StringBuilder builder = new StringBuilder();
+    for (int j = 0; j < ans.length(); ++j) {
+      int count = 1;
+      while (j + 1 < ans.length() && ans.charAt(j) == ans.charAt(j+1)) {
+        ++count;
+        ++j;
+      }
+      builder.append(count).append(ans.charAt(j));
+    }
+
+    return builder.toString();
   }
 
   public static void main(String[] args) {
