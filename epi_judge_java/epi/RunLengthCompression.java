e7b3ddff395759994a139f512d6faab8239f5274
jiangting <jiang.ting@northeastern.edu>
2021-08-12 11:29:46 -0700

    "decoding && encoding"

--- epi_judge_java/epi/RunLengthCompression.java
+++ epi_judge_java/epi/RunLengthCompression.java
@@ -6,11 +6,39 @@ public class RunLengthCompression {
 
   public static String decoding(String s) {
     // TODO - you fill in here.
-    return "";
+    int count = 0;
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (Character.isDigit(c)) {
+        count = count * 10 + c - '0';
+      } else {
+        while (count > 0) {
+          builder.append(c);
+          count--;
+        }
+
+      }
+    }
+    return builder.toString();
+
   }
+
   public static String encoding(String s) {
     // TODO - you fill in here.
-    return "";
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      int count = 1;
+      while (i+1 < s.length() && s.charAt(i) == s.charAt(i+1)) {
+        count ++;
+        i++;
+      }
+      builder.append(count).append(s.charAt(i));
+
+    }
+    return builder.toString();
+
+
   }
   @EpiTest(testDataFile = "run_length_compression.tsv")
   public static void rleTester(String encoded, String decoded)
