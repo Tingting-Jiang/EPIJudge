a5f8fe0e00b7d1772180e082c730b98d82abdb2b
jiangting <jiang.ting@northeastern.edu>
2021-08-12 11:20:44 -0700

    "getValidIpAddress"

--- epi_judge_java/epi/ValidIpAddresses.java
+++ epi_judge_java/epi/ValidIpAddresses.java
@@ -3,6 +3,7 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -11,7 +12,45 @@ public class ValidIpAddresses {
 
   public static List<String> getValidIpAddress(String s) {
     // TODO - you fill in here.
-    return null;
+
+    List<String> ans = new ArrayList<>();
+    for (int i = 1; i < 4 && i< s.length(); i++) {
+      String first = s.substring(0,i);
+
+      if (isValid(first)) {
+        for (int j = 1; j +i <s.length() && j<4; j++) {
+          String second = s.substring(i, i+j);
+
+          if (isValid(second)){
+            for (int k = 1; i+ k+j < s.length() && k<4 ; k++) {
+              String third = s.substring(i+j, k+j+i);
+              String fourth = s.substring(i+j+k);
+
+
+              if (isValid(third) && isValid(fourth)) {
+                ans.add(first+ "." + second + "." + third + "." + fourth);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return ans;
+
+
+
+  }
+
+
+  private static boolean isValid(String s) {
+
+    if (s.length() >= 4) return false;
+    if (s.startsWith("0") && s.length() >1) {
+      return false;
+    }
+    int val = Integer.parseInt(s);
+    return val <= 255 && val >= 0;
   }
   @EpiTestComparator
   public static boolean comp(List<String> expected, List<String> result) {
