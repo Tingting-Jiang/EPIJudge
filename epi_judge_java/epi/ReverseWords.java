5967343e6fce886c3d7865713252d7bc783063eb
jiangting <jiang.ting@northeastern.edu>
2021-08-12 09:55:39 -0700

    "reverseWords"

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -2,11 +2,41 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
+import java.util.Collections;
+
 public class ReverseWords {
 
   public static void reverseWords(char[] input) {
     // TODO - you fill in here.
-    return;
+    int left = 0, right = input.length;
+    reverseSingle(input, 0, right-1);
+    int start = 0;
+    int end = 0;
+
+    while (start < right ) {
+      while (start < end || start < right && input[start] == ' ') {
+        start ++; // skip empty space
+      }
+      while (end < start || end < right && input[end] != ' ') {
+        end ++; // skip non-spaces chars
+      }
+      reverseSingle(input, start, end-1);
+
+
+    }
+
+  }
+
+
+  public static  void reverseSingle(char[] word, int left, int right) {
+
+    while (left < right) {
+      char temp = word[left];
+      word[left++] = word[right];
+      word[right--] = temp;
+    }
+
   }
   @EpiTest(testDataFile = "reverse_words.tsv")
   public static String reverseWordsWrapper(TimedExecutor executor, String s)
