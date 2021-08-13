54de209dd439831d60a82bc3a562ef6926b6043e
jiangting <jiang.ting@northeastern.edu>
2021-08-13 16:06:38 -0700

    "eval"

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,12 +1,44 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.function.ToIntBiFunction;
+
 public class EvaluateRpn {
   @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
   public static int eval(String expression) {
     // TODO - you fill in here.
-    return 0;
+
+    Deque<Integer> stack = new ArrayDeque<>();
+    String DELIMITER = ",";
+    Map<String, ToIntBiFunction<Integer, Integer>> dic = Map.of(
+            "+",
+            (y, x)->(x + y),
+            "-",
+            (y, x)-> (x -y),
+            "*",
+            (y, x)->(x*y),
+            "/",
+            (y,x)->(x/y)
+    );
+
+    for (String word : expression.split(DELIMITER)) {
+      if (dic.get(word) != null) {
+        int val = dic.get(word).applyAsInt(stack.removeFirst(), stack.removeFirst());
+        stack.addFirst(val);
+      }
+      else {
+        stack.addFirst(Integer.parseInt(word));
+      }
+
+    }
+    return stack.removeFirst();
+
+
   }
 
   public static void main(String[] args) {
