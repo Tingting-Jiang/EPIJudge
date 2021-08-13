05b97de50f031e19cb841ca76ba806f8d3328f8f
jiangting <jiang.ting@northeastern.edu>
2021-08-13 09:07:10 -0700

    "isLinkedListAPalindrome"

--- epi_judge_java/epi/IsListPalindromic.java
+++ epi_judge_java/epi/IsListPalindromic.java
@@ -1,12 +1,47 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.List;
+
 public class IsListPalindromic {
   @EpiTest(testDataFile = "is_list_palindromic.tsv")
 
   public static boolean isLinkedListAPalindrome(ListNode<Integer> L) {
     // TODO - you fill in here.
+
+    ListNode<Integer> slow = L;
+    ListNode<Integer> fast = L;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    ListNode<Integer> start = L;
+    ListNode<Integer> half = reverse(slow);
+
+    while (half != null && start != null) {
+//      if (start.data != half.data) {
+      if (!(start.data.equals(half.data))) {
+        return false; // here must use equals to pass all tests
+      }
+      start = start.next;
+      half = half.next;
+    }
+
     return true;
+
+  }
+
+  private static ListNode<Integer> reverse(ListNode<Integer> head) {
+    ListNode<Integer> prev = null;
+    ListNode<Integer> curr = head;
+    while (curr != null) {
+      ListNode<Integer> succ = curr.next;
+      curr.next  = prev;
+      prev = curr;
+      curr = succ;
+    }
+    return prev;
   }
 
   public static void main(String[] args) {
