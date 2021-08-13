5ef7f24047f179f7d3f1a2d48823a91751290e8f
jiangting <jiang.ting@northeastern.edu>
2021-08-12 19:20:07 -0700

    "removeDuplicates"

--- epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
+++ epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
@@ -6,7 +6,16 @@ public class RemoveDuplicatesFromSortedList {
 
   public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
     // TODO - you fill in here.
-    return null;
+    ListNode<Integer> curr = L;
+    while (curr != null) {
+      ListNode<Integer> temp = curr.next;
+      while (temp != null && curr.data == temp.data) {
+        temp = temp.next;
+      }
+      curr.next = temp;
+      curr= temp;
+    }
+    return L;
   }
 
   public static void main(String[] args) {
