07e1fe60367c3c6efe3fa83062413de852b86600
jiangting <jiang.ting@northeastern.edu>
2021-08-13 16:23:21 -0700

    "isWellFormed"

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,12 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
     // TODO - you fill in here.
-    return true;
+
+    Map<Character, Character> dic = Map.of(
+            '(',')','{','}', '[',']'
+    );
+    Deque<Character> stack = new ArrayDeque<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (!stack.isEmpty() && dic.containsKey(stack.peek()) && dic.get(stack.peek()) == s.charAt(i)) {
+        stack.removeFirst();
+      }
+      else {
+        stack.addFirst(s.charAt(i));
+      }
+
+    }
+
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {
