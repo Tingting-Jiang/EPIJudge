aeed8e06b75adebe11c0bd9e914caf6961953225
jiangting <jiang.ting@northeastern.edu>
2021-08-13 10:26:25 -0700

    "addTwoNumbers"

--- epi_judge_java/epi/IntAsListAdd.java
+++ epi_judge_java/epi/IntAsListAdd.java
@@ -7,7 +7,22 @@ public class IntAsListAdd {
   public static ListNode<Integer> addTwoNumbers(ListNode<Integer> L1,
                                                 ListNode<Integer> L2) {
     // TODO - you fill in here.
-    return null;
+    ListNode<Integer> dummy = new ListNode<>(0, null);
+    ListNode<Integer> node = dummy;
+    int carry = 0;
+    while (L1 != null || L2 != null || carry != 0) {
+      carry += L1 == null ? 0 : L1.data;
+      L1 = L1 == null ? L1 : L1.next;
+      carry += L2 == null ? 0 : L2.data;
+      L2 = L2 == null ? L2 : L2.next;
+      int val =  carry % 10;
+      carry /= 10;
+      node.next = new ListNode<>(val, null);
+      node = node.next;
+
+    }
+
+    return dummy.next;
   }
 
   public static void main(String[] args) {
