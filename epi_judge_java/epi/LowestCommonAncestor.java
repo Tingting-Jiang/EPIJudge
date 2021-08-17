01afe5f7cd51589ed12267b1c0a7c14edcad499a
jiangting <jiang.ting@northeastern.edu>
2021-08-16 23:10:24 -0700

    "LowestCommonAncestor"

--- epi_judge_java/epi/LowestCommonAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestor.java
@@ -5,12 +5,41 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 public class LowestCommonAncestor {
+  private int num;
+  private BinaryTreeNode<Integer> node;
+
+  public LowestCommonAncestor(int num, BinaryTreeNode<Integer> node) {
+    this.num = num;
+    this.node = node;
+  }
+
   public static BinaryTreeNode<Integer> lca(BinaryTreeNode<Integer> tree,
                                             BinaryTreeNode<Integer> node0,
                                             BinaryTreeNode<Integer> node1) {
     // TODO - you fill in here.
-    return null;
+
+    return lcaHelper(tree, node0, node1).node;
+
   }
+
+  private static LowestCommonAncestor lcaHelper(BinaryTreeNode<Integer> tree,
+                                                   BinaryTreeNode<Integer> node0,
+                                                   BinaryTreeNode<Integer> node1) {
+
+    if (tree == null) return new LowestCommonAncestor(0, null);
+    LowestCommonAncestor left = lcaHelper(tree.left,node0, node1);
+    if (left.num == 2) return left;
+    LowestCommonAncestor right = lcaHelper(tree.right, node0, node1);
+    if (right.num == 2) return right;
+
+    int newNum = left.num + right.num + (tree == node0 ? 1: 0) + (tree == node1 ? 1:0);
+
+    return new LowestCommonAncestor(newNum, newNum == 2 ? tree: null);
+
+
+
+  }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor,
                                BinaryTreeNode<Integer> tree, Integer key0,
