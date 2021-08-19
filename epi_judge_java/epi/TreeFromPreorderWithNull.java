3463185eea551a9293c07d772f83cd22158bec3b
jiangting <jiang.ting@northeastern.edu>
2021-08-19 10:24:02 -0700

    "reconstructPreorder"

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -6,11 +6,27 @@ import epi.test_framework.TimedExecutor;
 import java.util.ArrayList;
 import java.util.List;
 public class TreeFromPreorderWithNull {
+  private static int idx;
+
+
   public static BinaryTreeNode<Integer>
   reconstructPreorder(List<Integer> preorder) {
     // TODO - you fill in here.
-    return null;
+    idx = 0;
+    return helper(preorder);
+
   }
+
+  private static BinaryTreeNode<Integer> helper(List<Integer> preorder) {
+    Integer rootVAal = preorder.get(idx);
+    idx ++;
+    if (rootVAal == null) return null;
+    BinaryTreeNode<Integer> left = helper(preorder);
+    BinaryTreeNode<Integer> right = helper(preorder);
+    return new BinaryTreeNode<>(rootVAal, left, right);
+
+  }
+
   @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
   public static BinaryTreeNode<Integer>
   reconstructPreorderWrapper(TimedExecutor executor, List<String> strings)
