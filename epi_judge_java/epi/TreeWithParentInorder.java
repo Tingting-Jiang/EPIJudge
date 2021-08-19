c4c2d02ea4b2e702fd79a08fa71f8dd19e40610c
jiangting <jiang.ting@northeastern.edu>
2021-08-19 09:58:36 -0700

    "inorderTraversal"

--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -2,13 +2,47 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class TreeWithParentInorder {
   @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
     // TODO - you fill in here.
- return null;
+      BinaryTree<Integer> prev = null;
+      BinaryTree<Integer> curr = tree;
+      List<Integer> ans = new ArrayList<>();
+      while(curr != null) {
+          BinaryTree<Integer> next;
+          if (curr.parent == prev) { // curr is the left child
+             if (curr.left != null) {
+                 next = curr.left;
+             }
+             else { // curr is leaf node: back to parent, or it has right child: traverse the right
+                 ans.add(curr.data);
+                 next = curr.right!= null ? curr.right : curr.parent;
+             }
+          }
+
+          else if (curr.left == prev) { // curr is the root, next traverse the right child
+              ans.add(curr.data);
+              if (curr.right != null) {
+                  next = curr.right;
+              } else {
+                  next = curr.parent;
+              }
+
+          } else { //curr is the right child
+              next = curr.parent;
+
+          }
+
+          prev = curr;
+          curr = next;
+
+
+      }
+      return ans;
 
   }
 
