8afc7a5ac1f8686b4a1c07674f0247b0ea76ce9c
jiangting <jiang.ting@northeastern.edu>
2021-08-19 11:00:44 -0700

    "exteriorBinaryTree"

--- epi_judge_java/epi/TreeExterior.java
+++ epi_judge_java/epi/TreeExterior.java
@@ -12,8 +12,59 @@ public class TreeExterior {
   public static List<BinaryTreeNode<Integer>>
   exteriorBinaryTree(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
-    return Collections.emptyList();
+
+    if (tree == null) return Collections.emptyList();
+    List<BinaryTreeNode<Integer>> ans = new ArrayList<>();
+    ans.add(tree);
+
+    leftExterior(tree.left, ans);
+    addLeaves(tree.left, ans);
+    addLeaves(tree.right, ans);
+    rightExterior(tree.right, ans);
+
+    return ans;
+
+
   }
+
+  private static void rightExterior(BinaryTreeNode<Integer> tree, List<BinaryTreeNode<Integer>> ans) {
+    if (tree == null || tree.left == null && tree.right == null) return;
+
+    if (tree.right != null) {
+      rightExterior(tree.right, ans);
+    } else {
+      rightExterior(tree.left, ans);
+    }
+    ans.add(tree);
+
+  }
+
+  private static void addLeaves(BinaryTreeNode<Integer> root, List<BinaryTreeNode<Integer>> ans) {
+    if (root != null) {
+      if (root.right == null && root.left == null) {
+        ans.add(root);
+      } else{
+        addLeaves(root.left, ans);
+        addLeaves(root.right, ans);
+      }
+    }
+  }
+
+  private static void leftExterior(BinaryTreeNode<Integer> tree, List<BinaryTreeNode<Integer>> ans) {
+    if (tree == null || tree.left == null && tree.right == null) return;
+    ans.add(tree);
+    if (tree.left!= null) {
+      leftExterior(tree.left, ans);
+    } else {
+      leftExterior(tree.right, ans);
+    }
+  }
+
+
+
+
+
+
   private static List<Integer> createOutputList(List<BinaryTreeNode<Integer>> L)
       throws TestFailure {
     if (L.contains(null)) {
