0b3af54557c0f252aff84e6ac213f7d281f24727
jiangting <jiang.ting@northeastern.edu>
2021-08-22 10:59:10 -0700

    "sortApproximatelySortedData"

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -2,14 +2,33 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SortAlmostSortedArray {
 
   public static List<Integer>
   sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
     // TODO - you fill in here.
-    return null;
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    List<Integer> ans = new ArrayList<>();
+    for (int i = 0; i < k+1 && sequence.hasNext(); i++) {
+      heap.add(sequence.next());
+
+    }
+    while (sequence.hasNext()) {
+      Integer smallest = heap.remove();
+      ans.add(smallest);
+      heap.add(sequence.next());
+    }
+//    ans.addAll(Stream.generate(heap::remove).limit(heap.size()).collect(Collectors.toList()));
+    while (!heap.isEmpty()) {
+      ans.add(heap.remove());
+    }
+
+    return ans;
   }
   @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
   public static List<Integer>
