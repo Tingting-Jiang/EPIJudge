de7de4213a24876a5332e351e2a7d6082a9bf33f
jiangting <jiang.ting@northeastern.edu>
2021-08-22 13:27:42 -0700

    "kLargestInBinaryHeap"

--- epi_judge_java/epi/KLargestInHeap.java
+++ epi_judge_java/epi/KLargestInHeap.java
@@ -6,13 +6,40 @@ import epi.test_framework.GenericTest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.function.BiPredicate;
 public class KLargestInHeap {
-  @EpiTest(testDataFile = "k_largest_in_heap.tsv")
+  private int value;
+  private int Id;
 
+  public KLargestInHeap(int value, int id) {
+    this.value = value;
+    Id = id;
+  }
+
+  private static final int CAPACITY = 16;
+  @EpiTest(testDataFile = "k_largest_in_heap.tsv")
   public static List<Integer> kLargestInBinaryHeap(List<Integer> A, int k) {
     // TODO - you fill in here.
-    return null;
+    if (k<= 0) return Collections.emptyList();
+
+
+    PriorityQueue<KLargestInHeap> heap = new PriorityQueue<>(
+            CAPACITY, (o1, o2) -> Integer.compare(o2.value, o1.value)
+    );
+    heap.add(new KLargestInHeap(A.get(0),0));
+
+    List<Integer> ans = new ArrayList<>();
+    for (int i = 0; i < k; i++) {
+      int idx = heap.peek().Id;
+      ans.add(heap.remove().value);
+
+      if (2*idx+1 < A.size()) heap.add(new KLargestInHeap(A.get(2*idx+1), 2*idx+1));
+      if (2*idx+2 < A.size()) heap.add(new KLargestInHeap(A.get(2*idx+2), 2*idx+2));
+
+    }
+
+    return ans;
   }
   @EpiTestComparator
   public static boolean comp(List<Integer> expected, List<Integer> result) {
