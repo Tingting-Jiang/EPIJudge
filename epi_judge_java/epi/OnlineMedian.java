9f3982614f6cce583596335eeb9b0f188659f6f9
jiangting <jiang.ting@northeastern.edu>
2021-08-22 11:39:43 -0700

    "onlineMedian"

--- epi_judge_java/epi/OnlineMedian.java
+++ epi_judge_java/epi/OnlineMedian.java
@@ -2,12 +2,30 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 public class OnlineMedian {
+  private static final int CAPACITY = 16;
   public static List<Double> onlineMedian(Iterator<Integer> sequence) {
     // TODO - you fill in here.
-    return null;
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(CAPACITY, Collections.reverseOrder());
+    List<Double> ans = new ArrayList<>();
+
+    while (sequence.hasNext()) {
+      int num = sequence.next();
+      minHeap.add(num);
+      maxHeap.add(minHeap.remove());
+      if (maxHeap.size() > minHeap.size()) {
+        minHeap.add(maxHeap.remove());
+      }
+      ans.add(minHeap.size() == maxHeap.size() ? (minHeap.peek() + maxHeap.peek()) * 0.5 : minHeap.peek());
+
+
+      }
+
+
+    return ans;
   }
   @EpiTest(testDataFile = "online_median.tsv")
   public static List<Double> onlineMedianWrapper(List<Integer> sequence) {
