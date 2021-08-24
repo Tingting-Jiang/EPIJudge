fd865744f34d8a8492398e0edd7afcef184766ed
jiangting <jiang.ting@northeastern.edu>
2021-08-23 22:21:12 -0700

    "matrixSearch"

--- epi_judge_java/epi/SearchRowColSortedMatrix.java
+++ epi_judge_java/epi/SearchRowColSortedMatrix.java
@@ -8,7 +8,19 @@ public class SearchRowColSortedMatrix {
 
   public static boolean matrixSearch(List<List<Integer>> A, int x) {
     // TODO - you fill in here.
-    return true;
+    if (x < A.get(0).get(0)) return false;
+    int row = 0, col = A.get(0).size()-1;
+    while (row <A.size() && col >=0) {
+      if (A.get(row).get(col) >x) {
+        col --;
+      } else if (A.get(row).get(col) < x) {
+        row ++;
+      } else {
+        return true;
+      }
+    }
+    return false;
+
   }
 
   public static void main(String[] args) {
