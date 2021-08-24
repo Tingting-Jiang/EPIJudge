86b733509853e5a55d44e6192ca5286a96450dc4
jiangting <jiang.ting@northeastern.edu>
2021-08-23 22:34:32 -0700

    "SearchForMinMaxInArray"

--- epi_judge_java/epi/SearchForMinMaxInArray.java
+++ epi_judge_java/epi/SearchForMinMaxInArray.java
@@ -47,7 +47,30 @@ public class SearchForMinMaxInArray {
 
   public static MinMax findMinMax(List<Integer> A) {
     // TODO - you fill in here.
-    return new MinMax(0, 0);
+    if (A.size()<=1) return new MinMax(A.get(0), A.get(0));
+//    int small = A.get(0) < A.get(1)? A.get(0): A.get(1);
+//    int large = small == A.get(0)? A.get(1) : A.get(0);
+//
+//    MinMax global = new MinMax(small, large);
+    MinMax global = MinMax.minMax(A.get(0), A.get(1));
+    for (int i = 2; i + 1 < A.size(); i+=2) {
+
+//      MinMax local = new MinMax(Math.min(A.get(i), A.get(i+1)), Math.max(A.get(i), A.get(i+1)));
+//      global = new MinMax(Math.min(global.smallest, local.smallest), Math.max(global.largest, local.largest));
+      MinMax local = MinMax.minMax(A.get(i), A.get(i+1));
+      global = new MinMax(Math.min(global.smallest, local.smallest),
+              Math.max(global.largest, local.largest));
+
+    }
+    if (A.size() % 2 != 0) {
+      global = new MinMax(Math.min(global.smallest, A.get(A.size()-1)),
+              Math.max(global.largest, A.get(A.size()-1)));
+
+    }
+
+
+
+    return global;
   }
 
   public static void main(String[] args) {
