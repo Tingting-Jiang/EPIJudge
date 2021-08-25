a9e6cb7f9f035548325b8e66498894d01c93cf1e
jiangting <jiang.ting@northeastern.edu>
2021-08-25 15:19:29 -0700

    "findNearestRepetition"

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -2,13 +2,24 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
   public static int findNearestRepetition(List<String> paragraph) {
     // TODO - you fill in here.
-    return 0;
+    int dif = Integer.MAX_VALUE;
+    HashMap<String, Integer> dic = new HashMap<>();
+    for( int i = 0; i <paragraph.size();i++) {
+      if (dic.containsKey(paragraph.get(i))) {
+        dif = Math.min(dif, i- dic.get(paragraph.get(i)));
+      }
+      dic.put(paragraph.get(i),i);
+    }
+
+
+    return dif == Integer.MAX_VALUE ? -1 : dif;
   }
 
   public static void main(String[] args) {
