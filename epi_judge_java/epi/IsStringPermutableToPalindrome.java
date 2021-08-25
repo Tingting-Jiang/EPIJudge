3044b98bb481fda2216b2de813ec35398ba6325c
jiangting <jiang.ting@northeastern.edu>
2021-08-25 13:42:06 -0700

    "canFormPalindrome"

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,12 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
     // TODO - you fill in here.
-    return true;
+    Set<Character> dic = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (dic.contains(c)) {
+        dic.remove(c);
+      } else {
+        dic.add(c);
+      }
+
+    }
+
+
+
+    return dic.size()<=1;
   }
 
   public static void main(String[] args) {
