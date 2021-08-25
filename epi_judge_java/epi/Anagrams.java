790893ec1cea7a25dd6ac81777244abfdf1e3dd3
jiangting <jiang.ting@northeastern.edu>
2021-08-25 13:28:18 -0700

    "findAnagrams"

--- epi_judge_java/epi/Anagrams.java
+++ epi_judge_java/epi/Anagrams.java
@@ -4,15 +4,27 @@ import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Anagrams {
   @EpiTest(testDataFile = "anagrams.tsv")
 
   public static List<List<String>> findAnagrams(List<String> dictionary) {
     // TODO - you fill in here.
-    return null;
+
+    HashMap<String, List<String>> dic = new HashMap<>();
+    for (String word: dictionary) {
+      String sorted = Stream.of(word.split("")).sorted().collect(Collectors.joining());
+      dic.putIfAbsent(sorted, new ArrayList<String>());
+      dic.get(sorted).add(word);
+    }
+    return dic.values().stream().filter(group->group.size() >=2).collect(Collectors.toList());
   }
   @EpiTestComparator
   public static boolean comp(List<List<String>> expected,
