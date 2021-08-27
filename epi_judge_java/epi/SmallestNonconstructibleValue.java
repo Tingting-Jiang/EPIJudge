321ea6582199efac0ee9d09cf2c963de71568d1b
jiangting <jiang.ting@northeastern.edu>
2021-08-27 16:15:16 -0700

    "smallestNonconstructibleValue"

--- epi_judge_java/epi/SmallestNonconstructibleValue.java
+++ epi_judge_java/epi/SmallestNonconstructibleValue.java
@@ -2,13 +2,26 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class SmallestNonconstructibleValue {
   @EpiTest(testDataFile = "smallest_nonconstructible_value.tsv")
 
   public static int smallestNonconstructibleValue(List<Integer> A) {
     // TODO - you fill in here.
-    return 0;
+
+    Collections.sort(A);
+    int num = 0;
+    for (int a: A) {
+      if (a > num+1) {
+        break ;
+      }
+      else {
+        num += a;
+      }
+
+    }
+    return num+1;
   }
 
   public static void main(String[] args) {
