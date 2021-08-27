d74ced797cb774730cc80225bab6b9c43f03aadf
jiangting <jiang.ting@northeastern.edu>
2021-08-27 15:56:48 -0700

    "hIndex"

--- epi_judge_java/epi/HIndex.java
+++ epi_judge_java/epi/HIndex.java
@@ -2,11 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class HIndex {
   @EpiTest(testDataFile = "h_index.tsv")
   public static int hIndex(List<Integer> citations) {
     // TODO - you fill in here.
+    Collections.sort(citations);
+    int length = citations.size();
+    for (int i = 0; i < length ; i++) {
+      if (citations.get(i) >= length - i) {
+        return length - i;
+      }
+
+    }
+
     return 0;
   }
 
