31b66af920c23a8e17a693ae0304460bc2bdeb3b
jiangting <jiang.ting@northeastern.edu>
2021-08-27 15:26:32 -0700

    "intersectTwoSortedArrays"

--- epi_judge_java/epi/IntersectSortedArrays.java
+++ epi_judge_java/epi/IntersectSortedArrays.java
@@ -14,13 +14,32 @@ public class IntersectSortedArrays {
                                                        List<Integer> B) {
     // TODO - you fill in here.
     List<Integer> ans = new ArrayList<>();
-    for (int i = 0; i < A.size(); i++) {
-      if ((i == 0 || !A.get(i).equals(A.get(i-1)))
-              && Collections.binarySearch(B, A.get(i)) >=0)  {
+//    for (int i = 0; i < A.size(); i++) {
+//      if ((i == 0 || !A.get(i).equals(A.get(i-1)))
+//              && Collections.binarySearch(B, A.get(i)) >=0)  {
+//        ans.add(A.get(i));
+//      }
+//
+//    }
+
+
+
+    int i = 0, j = 0;
+
+    while (i < A.size() && j < B.size()) {
+      if (A.get(i).equals(B.get(j)) &&(i == 0 || !A.get(i).equals(A.get(i-1)))) {
         ans.add(A.get(i));
+        i++;
+        j++;
+      }
+      else if (A.get(i) < B.get(j)) {
+        i++;
+      } else {
+        j++;
       }
 
     }
+
 
     return ans;
   }
