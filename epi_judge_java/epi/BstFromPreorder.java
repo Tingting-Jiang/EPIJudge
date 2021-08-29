3a7362f108b403ef538914121132ebb68837f583
jiangting <jiang.ting@northeastern.edu>
2021-08-29 11:41:53 -0700

    "rebuildHelper"

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -4,12 +4,48 @@ import epi.test_framework.GenericTest;
 
 import java.util.List;
 public class BstFromPreorder {
+  private static int rootIdx;
   @EpiTest(testDataFile = "bst_from_preorder.tsv")
+
+//  public static BstNode<Integer>
+//  rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+//    // TODO - you fill in here.
+//
+//    return rebuildHelper(preorderSequence, 0, preorderSequence.size());
+//
+//  }
+//
+//  private static BstNode<Integer> rebuildHelper(List<Integer> preorderSequence, int start, int end) {
+//    if (start >= end) return null;
+//    int trans = start + 1;
+//    while (trans < end && preorderSequence.get(trans) < preorderSequence.get(start) ) {
+//      trans++;
+//    }
+//    return new BstNode<>(preorderSequence.get(start),
+//            rebuildHelper(preorderSequence, start+1, trans),
+//            rebuildHelper(preorderSequence, trans, end));
+//
+//  }
+
 
   public static BstNode<Integer>
   rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-    // TODO - you fill in here.
-    return null;
+    rootIdx = 0;
+    return rebuildHelper(preorderSequence,Integer.MAX_VALUE, Integer.MIN_VALUE);
+  }
+
+  private static BstNode<Integer> rebuildHelper(List<Integer> preorderSequence, int upper, int lower) {
+    if (rootIdx == preorderSequence.size()) return null;
+    int rootVal = preorderSequence.get(rootIdx);
+    if (rootVal> upper || rootVal < lower) return null;
+
+    rootIdx++;
+    BstNode<Integer> left = rebuildHelper(preorderSequence, rootVal, lower);
+    BstNode<Integer> right = rebuildHelper(preorderSequence, upper, rootVal);
+
+    return new BstNode<>(rootVal, left, right);
+
+
   }
 
   public static void main(String[] args) {
