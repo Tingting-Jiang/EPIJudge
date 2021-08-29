c0e254209b22f97125883a86178c3151dff49790
jiangting <jiang.ting@northeastern.edu>
2021-08-28 17:45:13 -0700

    "stableSortList"

--- epi_judge_java/epi/GroupEqualEntries.java
+++ epi_judge_java/epi/GroupEqualEntries.java
@@ -5,11 +5,8 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 public class GroupEqualEntries {
   @EpiUserType(ctorParams = {Integer.class, String.class})
 
@@ -45,7 +42,36 @@ public class GroupEqualEntries {
   }
   public static void groupByAge(List<Person> people) {
     // TODO - you fill in here.
-    return;
+
+    Map<Integer, Integer> ageToCount = new HashMap<>();
+    for(Person p : people) {
+      ageToCount.put(p.age, ageToCount.getOrDefault(p.age, 0) + 1);
+    }
+
+    Map<Integer, Integer> ageToOffset = new HashMap<>();
+    int offset = 0;
+    for(Map.Entry<Integer, Integer> kc: ageToCount.entrySet()) {
+      ageToOffset.put(kc.getKey(), offset);
+      offset += kc.getValue();
+    }
+
+    while (!ageToOffset.isEmpty()) {
+      Map.Entry<Integer, Integer> from = ageToOffset.entrySet().iterator().next();
+      Integer toAge = people.get(from.getValue()).age;
+      Integer toValue = ageToOffset.get(toAge);
+      Collections.swap(people, from.getValue(), toValue);
+
+      Integer count = ageToCount.get(toAge)-1;
+      ageToCount.put(toAge, count);
+
+      if (count > 0) {
+        ageToOffset.put(toAge, toValue+1);
+      } else {
+        ageToOffset.remove(toAge);
+      }
+    }
+
+
   }
   private static Map<Person, Integer> buildMultiset(List<Person> people) {
     Map<Person, Integer> m = new HashMap<>();
