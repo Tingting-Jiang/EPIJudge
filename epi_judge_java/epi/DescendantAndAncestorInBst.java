b3e1956fc0f900cb91f192d266e76ab4789b5942
jiangting <jiang.ting@northeastern.edu>
2021-08-29 16:17:59 -0700

    "pairIncludesAncestorAndDescendantOfM"

--- epi_judge_java/epi/DescendantAndAncestorInBst.java
+++ epi_judge_java/epi/DescendantAndAncestorInBst.java
@@ -10,8 +10,39 @@ public class DescendantAndAncestorInBst {
                                        BstNode<Integer> possibleAncOrDesc1,
                                        BstNode<Integer> middle) {
     // TODO - you fill in here.
-    return true;
+    BstNode<Integer> search0 = possibleAncOrDesc0, search1 = possibleAncOrDesc1;
+    while (search0 != possibleAncOrDesc1 && search0 != middle
+            && search1 != possibleAncOrDesc0 && search1 != middle
+            && (search0 != null || search1 != null)) {
+      if (search0 != null) {
+        search0 = search0.data > middle.data ? search0.left : search0.right;
+      }
+
+      if (search1 != null) {
+        search1 = search1.data > middle.data ? search1.left : search1.right;
+      }
+    }
+
+    if (search0 == possibleAncOrDesc1 || search1 == possibleAncOrDesc0 ||
+            (search0 != middle && search1 != middle)) {
+      return false;
+    }
+
+
+//    return helper(middle, search0 == middle ? possibleAncOrDesc1: possibleAncOrDesc0 );
+    return search0 == middle ? helper(middle, possibleAncOrDesc1)
+                              : helper(middle, possibleAncOrDesc0);
+
   }
+
+  private static boolean helper(BstNode<Integer> middle, BstNode<Integer> toNode) {
+    while (middle != null && middle != toNode) {
+      middle = middle.data > toNode.data ? middle.left : middle.right;
+
+    }
+    return middle == toNode;
+  }
+
   @EpiTest(testDataFile = "descendant_and_ancestor_in_bst.tsv")
   public static boolean pairIncludesAncestorAndDescendantOfMWrapper(
       TimedExecutor executor, BstNode<Integer> tree, int possibleAncOrDesc0,
--- problem_mapping.js
+++ problem_mapping.js
@@ -1912,7 +1912,7 @@ problem_mapping = {
                 "total": 958
             },
             "Java: DescendantAndAncestorInBst.java": {
-                "passed": 0,
+                "passed": 958,
                 "total": 958
             },
             "Python: descendant_and_ancestor_in_bst.py": {
