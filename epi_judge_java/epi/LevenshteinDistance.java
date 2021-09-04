2da07042b75ce7dbbd6477ef4300783210222ea7
jiangting <jiang.ting@northeastern.edu>
2021-09-03 17:17:27 -0700

    "levenshteinDistance"

--- epi_judge_java/epi/LevenshteinDistance.java
+++ epi_judge_java/epi/LevenshteinDistance.java
@@ -1,12 +1,39 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Arrays;
+
 public class LevenshteinDistance {
   @EpiTest(testDataFile = "levenshtein_distance.tsv")
 
   public static int levenshteinDistance(String A, String B) {
     // TODO - you fill in here.
-    return 0;
+    int [][] dic = new int[A.length()][B.length()];
+    for(int[] row: dic) {
+      Arrays.fill(row, -1);
+    }
+
+    return distanceHelper(A, A.length()-1, B, B.length()-1, dic);
+
+  }
+
+  private static int distanceHelper(String A, int aIndx, String B, int bIndx, int[][] dic) {
+    if(aIndx < 0) return bIndx+1;
+
+    else if (bIndx<0) return aIndx+1;
+
+    if(dic[aIndx][bIndx] == -1) {
+      if(A.charAt(aIndx) == B.charAt(bIndx)) {
+        dic[aIndx][bIndx] = distanceHelper(A, aIndx-1, B, bIndx-1, dic);
+      } else {
+        int substitute = distanceHelper(A, aIndx-1, B, bIndx-1, dic);
+        int addLast = distanceHelper(A, aIndx,B, bIndx-1, dic);
+        int delete = distanceHelper(A, aIndx-1, B, bIndx, dic);
+        dic[aIndx][bIndx] = 1+ Math.min(substitute, Math.min(addLast, delete));
+      }
+    }
+    return dic[aIndx][bIndx];
   }
 
   public static void main(String[] args) {
