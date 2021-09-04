3d11c6fe4be526dad0f1cc1acd599f224b60a8fa
jiangting <jiang.ting@northeastern.edu>
2021-09-03 20:50:42 -0700

    "numberOfWays"

--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -6,7 +6,20 @@ public class NumberOfTraversalsMatrix {
 
   public static int numberOfWays(int n, int m) {
     // TODO - you fill in here.
-    return 0;
+
+    return waysHelper(n-1, m-1, new int[n][m]);
+  }
+
+  private static int waysHelper(int row, int col, int[][] ints) {
+    if (row == 0 && col == 0) {
+      return 1;
+    }
+    if (ints[row][col] == 0) {
+      int waysTop = row == 0? 0: waysHelper(row-1, col, ints);
+      int waysLeft = col == 0? 0: waysHelper(row, col-1, ints);
+      ints[row][col] = waysLeft + waysTop;
+    }
+    return ints[row][col];
   }
 
   public static void main(String[] args) {
