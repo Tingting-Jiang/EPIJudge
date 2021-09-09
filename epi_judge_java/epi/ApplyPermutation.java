0b0da717892353b455dc6b854c70e291d64108b4
jiangting <jiang.ting@northeastern.edu>
2021-09-09 11:42:07 -0700

    "applyPermutation"

--- epi_judge_java/epi/ApplyPermutation.java
+++ epi_judge_java/epi/ApplyPermutation.java
@@ -2,10 +2,20 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class ApplyPermutation {
   public static void applyPermutation(List<Integer> perm, List<Integer> A) {
     // TODO - you fill in here.
+    for (int i = 0; i < A.size(); i++) {
+      while (perm.get(i) != i) {
+        Collections.swap(A, i, perm.get(i));
+        Collections.swap(perm, i, perm.get(i));
+
+      }
+
+    }
+
     return;
   }
   @EpiTest(testDataFile = "apply_permutation.tsv")
--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -10,25 +10,24 @@ public class PrimeSieve {
   // Given n, return all primes up to and including n.
   public static List<Integer> generatePrimes(int n) {
     // TODO - you fill in here.
-    List<Boolean> table = new ArrayList<>(Collections.nCopies(n+1,true));
-    List<Integer> ans = new ArrayList<>();
-    table.set(0,false);
+    List<Integer> res = new ArrayList<>();
+    List<Boolean> table = new ArrayList<>(Collections.nCopies(n+1, true));
+    table.set(0, false);
     table.set(1, false);
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <=n; i++) {
       if (table.get(i)) {
-        ans.add(i);
+        res.add(i);
+        for (int j = i * 2; j <= n; j+= i) {
+          table.set(j, false);
 
-        // set all this primes multiplies to false
-        for (int k = i; k <= n; k += i) {
-          table.set(k, false);
         }
       }
-    }
 
-    return ans;
+    }
 
     // Time: should be nloglogn    Space: n
 
+    return res;
 
 
   }
