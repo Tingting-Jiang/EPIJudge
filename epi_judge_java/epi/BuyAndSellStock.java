a603bb814a7971a63ab247efa5cf7972b5695709
jiangting <jiang.ting@northeastern.edu>
2021-09-09 10:27:21 -0700

    "computeMaxProfit"

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -7,16 +7,15 @@ public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
     // TODO - you fill in here.
-
-    double maxProfit = 0.0;
-    double minPrice = Double.MAX_VALUE;
-    for (double price: prices) {
-      minPrice = Math.min(minPrice, price);
-      maxProfit = Math.max(maxProfit, price - minPrice);
+    double earn = 0.0;
+    double price = Double.MAX_VALUE;
+    for (double each: prices) {
+      price = Math.min(price, each);
+      earn = Math.max(earn, each - price);
 
     }
-    return maxProfit;
 
+    return earn;
 
 
   }
