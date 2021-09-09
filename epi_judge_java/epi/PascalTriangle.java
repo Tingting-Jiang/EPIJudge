d54cce7a30598a7ae7a75c63eb8c03b6ab40481a
jiangting <jiang.ting@northeastern.edu>
2021-09-09 15:52:03 -0700

    "generatePascalTriangle"

--- epi_judge_java/epi/PascalTriangle.java
+++ epi_judge_java/epi/PascalTriangle.java
@@ -2,13 +2,24 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class PascalTriangle {
   @EpiTest(testDataFile = "pascal_triangle.tsv")
 
   public static List<List<Integer>> generatePascalTriangle(int numRows) {
     // TODO - you fill in here.
-    return null;
+    List<List<Integer>> res = new ArrayList<>();
+    for (int i = 0; i < numRows; i++) {
+      List<Integer> currRow = new ArrayList<>();
+      for (int j = 0; j <= i; j++) {
+        currRow.add((0<j && j< i) ? res.get(i-1).get(j-1) +
+                res.get(i-1).get(j) : 1);
+
+      }
+      res.add(currRow);
+    }
+    return res;
   }
 
   public static void main(String[] args) {
