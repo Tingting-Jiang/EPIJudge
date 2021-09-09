2947f68371b8d564fe6bf3e54c0e501784d3dba8
jiangting <jiang.ting@northeastern.edu>
2021-09-09 15:48:16 -0700

    "matrixInSpiralOrder"

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -8,26 +8,21 @@ public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
     // TODO - you fill in here.
-    int k = perm.size()-2;
-
-// minus the k until requirement meet, "=" sign also matters
-    while (k>=0 && perm.get(k) >= perm.get(k+1)) {
+    int k = perm.size() -2;
+    while(k >= 0 && perm.get(k) >= perm.get(k+1)) {
       k--;
     }
 
-    if (k==-1) {
-      return Collections.emptyList();
-    }
+    if (k == -1) return Collections.emptyList();
 
-    for (int i = perm.size()-1; i > k ; i--) {
-      if (perm.get(k) <perm.get(i)) {
-        Collections.swap(perm, i, k);
-        break;
-      }
+    int small = perm.size()-1;
+    while (small > k && perm.get(small) <= perm.get(k))  {
+      small--;
     }
-
+    Collections.swap(perm, k, small);
     Collections.reverse(perm.subList(k+1, perm.size()));
     return perm;
+
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/SpiralOrdering.java
+++ epi_judge_java/epi/SpiralOrdering.java
@@ -2,14 +2,53 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class SpiralOrdering {
   @EpiTest(testDataFile = "spiral_ordering.tsv")
 
   public static List<Integer>
-  matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
+  matrixInSpiralOrder(List<List<Integer>> A) {
     // TODO - you fill in here.
-    return null;
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < Math.ceil(0.5 * A.size()); i++) {
+      matrixHelper(A,res,i);
+    }
+
+    return res;
+  }
+
+  private static void matrixHelper(List<List<Integer>> A, List<Integer> res, int offset) {
+    if (offset == A.size() - offset - 1) {
+      res.add(A.get(offset).get(offset));
+      return;
+    }
+
+    // top
+    for (int i = offset; i <A.size() - 1 - offset ; i++) {
+      res.add(A.get(offset).get(i));
+    }
+
+    // right
+    for (int j = offset; j < A.size() - 1 - offset; j++) {
+      res.add(A.get(j).get(A.size() - 1 - offset));
+
+    }
+
+    // bottom
+    for (int k = A.size()- offset - 1; k > offset  ; k--) {
+      res.add(A.get(A.size()- offset - 1).get(k));
+
+    }
+
+    // left
+    for (int m = A.size()- offset - 1; m > offset  ; m--) {
+      res.add(A.get(m).get(offset));
+
+    }
+
+
+
   }
 
   public static void main(String[] args) {
